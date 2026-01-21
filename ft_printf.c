8ad8a2fe77c9c1ed96cf4cd1173f8761e6fdc19a
Yoann <neuillemacgg@gmail.com>
2026-01-21 17:53:30 +0100

    compre putnbr & ptnbrbase

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: vasari <vasari@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 15:10:20 by vasari            #+#    #+#             */
-/*   Updated: 2025/12/05 15:12:31 by vasari           ###   ########.fr       */
+/*   Updated: 2026/01/21 17:28:32 by vasari           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,38 @@
 #include <stdio.h>
 #include <unistd.h>
 
+// void ft_putchar(char c)
+// {
+//     write(1, &c, 1);
+// 
+
 int ft_putnbr(int n)
 {
-    // Utilise ft_putnbr_base avec la base 10
+    if (n == -2147483648)
+        write(1, "-2147483648", 21);
+    if (n < 0)
+    {
+        n = n * -1;
+        write(1, "-", 1);
+    }
     
-    // Attention aux nombres négatifs !
-    // Retourne le nombre de caractères affichés
+    if (n >= 10)
+    {
+        ft_putnbr(n / 10);
+        ft_putnbr(n % 10);
+    }
+    
+    if (n <= 9)
+    {
+        n = n + '0';
+        write(1, &n, 1);
+    }
+    return (n); 
+}
+
+int main ()
+{
+    int a = -123;
+    ft_putnbr(123);
+    return 0;
 }
--- ft_putnbr_base.c
+++ ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: vasari <vasari@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 15:10:32 by vasari            #+#    #+#             */
-/*   Updated: 2025/12/05 15:10:32 by vasari           ###   ########.fr       */
+/*   Updated: 2026/01/19 20:30:15 by vasari           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,19 +52,19 @@ int	ft_putnbr_base(long nbr, char *base)
 	return (len);
 }
 
-// int	main(void)
-// {
-// 	char	dec[] = "0123456789";
-// 	char	hexa[] = "0123456789abcdef";
-// 	int		len_dec;
-// 	int		len_hexa;
+int	main(void)
+{
+	char	dec[] = "0123456789";
+	char	hexa[] = "0123456789abcdef";
+	int		len_dec;
+	int		len_hexa;
 
-// 	len_dec = ft_putnbr_base(634213, dec);
-// 	write(1, "\n", 1);
-// 	len_hexa = ft_putnbr_base(8327, hexa);
-// 	write(1, "\n", 1);
-// 	return (0);
-// }
+	len_dec = ft_putnbr_base(634213, dec);
+	write(1, "\n", 1);
+	len_hexa = ft_putnbr_base(8327, hexa);
+	write(1, "\n", 1);
+	return (0);
+}
 
 // /*
 //     dans un int foutre la taille de basse avec un strlen
